336f2498774c76bb8eee091df677adc6c36b382d
john-royal <34844819+john-royal@users.noreply.github.com>
2023-03-31 18:18:05 -0400

    style: format files

--- chat.cpp
+++ chat.cpp
@@ -22,16 +22,17 @@ using std::deque;
 using std::pair;
 #include "dh.h"
 
-static pthread_t trecv;     /* wait for incoming messagess and post to queue */
-void* recvMsg(void*);       /* for trecv */
-static pthread_t tcurses;   /* setup curses and draw messages from queue */
-void* cursesthread(void*);  /* for tcurses */
+static pthread_t trecv;		/* wait for incoming messagess and post to queue */
+void *recvMsg(void *);		/* for trecv */
+static pthread_t tcurses;	/* setup curses and draw messages from queue */
+void *cursesthread(void *); /* for tcurses */
 /* tcurses will get a queue full of these and redraw the appropriate windows */
-struct redraw_data {
+struct redraw_data
+{
 	bool resize;
 	string msg;
 	string sender;
-	WINDOW* win;
+	WINDOW *win;
 };
 static deque<redraw_data> mq; /* messages and resizes yet to be drawn */
 /* manage access to message queue: */
@@ -43,7 +44,7 @@ static pthread_cond_t qcv = PTHREAD_COND_INITIALIZER;
 /* record chat history as deque of strings: */
 static deque<string> transcript;
 
-#define max(a, b)         \
+#define max(a, b) \
 	({ typeof(a) _a = a;    \
 	 typeof(b) _b = b;    \
 	 _a > _b ? _a : _b; })
@@ -69,17 +70,17 @@ int initServerNet(int port)
 	/* NOTE: might not need the above if you make sure the client closes first */
 	if (listensock < 0)
 		error("ERROR opening socket");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	bzero((char *)&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(port);
-	if (bind(listensock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	if (bind(listensock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR on binding");
-	fprintf(stderr, "listening on port %i...\n",port);
-	listen(listensock,1);
+	fprintf(stderr, "listening on port %i...\n", port);
+	listen(listensock, 1);
 	socklen_t clilen;
-	struct sockaddr_in  cli_addr;
-	sockfd = accept(listensock, (struct sockaddr *) &cli_addr, &clilen);
+	struct sockaddr_in cli_addr;
+	sockfd = accept(listensock, (struct sockaddr *)&cli_addr, &clilen);
 	if (sockfd < 0)
 		error("error on accept");
 	close(listensock);
@@ -88,8 +89,7 @@ int initServerNet(int port)
 	return 0;
 }
 
-
-static int initClientNet(char* hostname, int port)
+static int initClientNet(char *hostname, int port)
 {
 	struct sockaddr_in serv_addr;
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -97,15 +97,16 @@ static int initClientNet(char* hostname, int port)
 	if (sockfd < 0)
 		error("ERROR opening socket");
 	server = gethostbyname(hostname);
-	if (server == NULL) {
-		fprintf(stderr,"ERROR, no such host\n");
+	if (server == NULL)
+	{
+		fprintf(stderr, "ERROR, no such host\n");
 		exit(0);
 	}
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	bzero((char *)&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+	memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
 	serv_addr.sin_port = htons(port);
-	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR connecting");
 	/* at this point, should be able to send/recv on sockfd */
 	return 0;
@@ -113,18 +114,18 @@ static int initClientNet(char* hostname, int port)
 
 static int shutdownNetwork()
 {
-	shutdown(sockfd,2);
+	shutdown(sockfd, 2);
 	unsigned char dummy[64];
 	ssize_t r;
-	do {
-		r = recv(sockfd,dummy,64,0);
+	do
+	{
+		r = recv(sockfd, dummy, 64, 0);
 	} while (r != 0 && r != -1);
 	close(sockfd);
 	return 0;
 }
 
 /* end network stuff. */
-
 
 [[noreturn]] static void fail_exit(const char *msg)
 {
@@ -138,10 +139,10 @@ static int shutdownNetwork()
 
 // Checks errors for (most) ncurses functions. CHECK(fn, x, y, z) is a checked
 // version of fn(x, y, z).
-#define CHECK(fn, ...) \
-	do \
-	if (fn(__VA_ARGS__) == ERR) \
-	fail_exit(#fn"("#__VA_ARGS__") failed"); \
+#define CHECK(fn, ...)                                  \
+	do                                                  \
+		if (fn(__VA_ARGS__) == ERR)                     \
+			fail_exit(#fn "(" #__VA_ARGS__ ") failed"); \
 	while (false)
 
 static bool should_exit = false;
@@ -163,15 +164,16 @@ static int readline_getc(FILE *dummy)
 
 /* if batch is set, don't draw immediately to real screen (use wnoutrefresh
  * instead of wrefresh) */
-static void msg_win_redisplay(bool batch, const string& newmsg="", const string& sender="")
+static void msg_win_redisplay(bool batch, const string &newmsg = "", const string &sender = "")
 {
 	if (batch)
 		wnoutrefresh(msg_win);
-	else {
-		wattron(msg_win,COLOR_PAIR(2));
-		wprintw(msg_win,"%s:",sender.c_str());
-		wattroff(msg_win,COLOR_PAIR(2));
-		wprintw(msg_win," %s\n",newmsg.c_str());
+	else
+	{
+		wattron(msg_win, COLOR_PAIR(2));
+		wprintw(msg_win, "%s:", sender.c_str());
+		wattroff(msg_win, COLOR_PAIR(2));
+		wprintw(msg_win, " %s\n", newmsg.c_str());
 		wrefresh(msg_win);
 	}
 }
@@ -179,22 +181,26 @@ static void msg_win_redisplay(bool batch, const string& newmsg="", const string&
 static void msg_typed(char *line)
 {
 	string mymsg;
-	if (!line) {
+	if (!line)
+	{
 		// Ctrl-D pressed on empty line
 		should_exit = true;
 		/* XXX send a "goodbye" message so other end doesn't
 		 * have to wait for timeout on recv()? */
-	} else {
-		if (*line) {
+	}
+	else
+	{
+		if (*line)
+		{
 			add_history(line);
 			mymsg = string(line);
 			transcript.push_back("me: " + mymsg);
 			ssize_t nbytes;
-			if ((nbytes = send(sockfd,line,mymsg.length(),0)) == -1)
+			if ((nbytes = send(sockfd, line, mymsg.length(), 0)) == -1)
 				error("send failed");
 		}
 		pthread_mutex_lock(&qmx);
-		mq.push_back({false,mymsg,"me",msg_win});
+		mq.push_back({false, mymsg, "me", msg_win});
 		pthread_cond_signal(&qcv);
 		pthread_mutex_unlock(&qmx);
 	}
@@ -205,17 +211,20 @@ static void msg_typed(char *line)
 static void cmd_win_redisplay(bool batch)
 {
 	int prompt_width = strnlen(rl_display_prompt, 128);
-	int cursor_col = prompt_width + strnlen(rl_line_buffer,rl_point);
+	int cursor_col = prompt_width + strnlen(rl_line_buffer, rl_point);
 
 	werase(cmd_win);
 	mvwprintw(cmd_win, 0, 0, "%s%s", rl_display_prompt, rl_line_buffer);
 	/* XXX deal with a longer message than the terminal window can show */
-	if (cursor_col >= COLS) {
+	if (cursor_col >= COLS)
+	{
 		// Hide the cursor if it lies outside the window. Otherwise it'll
 		// appear on the very right.
 		curs_set(0);
-	} else {
-		wmove(cmd_win,0,cursor_col);
+	}
+	else
+	{
+		wmove(cmd_win, 0, cursor_col);
 		curs_set(1);
 	}
 	if (batch)
@@ -227,20 +236,21 @@ static void cmd_win_redisplay(bool batch)
 static void readline_redisplay(void)
 {
 	pthread_mutex_lock(&qmx);
-	mq.push_back({false,"","",cmd_win});
+	mq.push_back({false, "", "", cmd_win});
 	pthread_cond_signal(&qcv);
 	pthread_mutex_unlock(&qmx);
 }
 
 static void resize(void)
 {
-	if (LINES >= 3) {
-		wresize(msg_win,LINES-2,COLS);
-		wresize(sep_win,1,COLS);
-		wresize(cmd_win,1,COLS);
+	if (LINES >= 3)
+	{
+		wresize(msg_win, LINES - 2, COLS);
+		wresize(sep_win, 1, COLS);
+		wresize(cmd_win, 1, COLS);
 		/* now move bottom two to last lines: */
-		mvwin(sep_win,LINES-2,0);
-		mvwin(cmd_win,LINES-1,0);
+		mvwin(sep_win, LINES - 2, 0);
+		mvwin(cmd_win, LINES - 1, 0);
 	}
 
 	/* Batch refreshes and commit them with doupdate() */
@@ -255,7 +265,8 @@ static void init_ncurses(void)
 	if (!initscr())
 		fail_exit("Failed to initialize ncurses");
 
-	if (has_colors()) {
+	if (has_colors())
+	{
 		CHECK(start_color);
 		CHECK(use_default_colors);
 	}
@@ -266,11 +277,14 @@ static void init_ncurses(void)
 
 	curs_set(1);
 
-	if (LINES >= 3) {
+	if (LINES >= 3)
+	{
 		msg_win = newwin(LINES - 2, COLS, 0, 0);
 		sep_win = newwin(1, COLS, LINES - 2, 0);
 		cmd_win = newwin(1, COLS, LINES - 1, 0);
-	} else {
+	}
+	else
+	{
 		// Degenerate case. Give the windows the minimum workable size to
 		// prevent errors from e.g. wmove().
 		msg_win = newwin(1, COLS, 0, 0);
@@ -280,9 +294,10 @@ static void init_ncurses(void)
 	if (!msg_win || !sep_win || !cmd_win)
 		fail_exit("Failed to allocate windows");
 
-	scrollok(msg_win,true);
+	scrollok(msg_win, true);
 
-	if (has_colors()) {
+	if (has_colors())
+	{
 		// Use white-on-blue cells for the separator window...
 		CHECK(init_pair, 1, COLOR_WHITE, COLOR_BLUE);
 		CHECK(wbkgd, sep_win, COLOR_PAIR(1));
@@ -290,8 +305,9 @@ static void init_ncurses(void)
 		 * not appear to be any of the normal colors curses defines. */
 		CHECK(init_pair, 2, COLOR_MAGENTA, -1);
 	}
-	else {
-		wbkgd(sep_win,A_STANDOUT); /* c.f. man curs_attr */
+	else
+	{
+		wbkgd(sep_win, A_STANDOUT); /* c.f. man curs_attr */
 	}
 	wrefresh(sep_win);
 }
@@ -331,55 +347,59 @@ static void deinit_readline(void)
 	rl_callback_handler_remove();
 }
 
-static const char* usage =
-"Usage: %s [OPTIONS]...\n"
-"Secure chat for CSc380.\n\n"
-"   -c, --connect HOST  Attempt a connection to HOST.\n"
-"   -l, --listen        Listen for new connections.\n"
-"   -p, --port    PORT  Listen or connect on PORT (defaults to 1337).\n"
-"   -h, --help          show this message and exit.\n";
+static const char *usage =
+	"Usage: %s [OPTIONS]...\n"
+	"Secure chat for CSc380.\n\n"
+	"   -c, --connect HOST  Attempt a connection to HOST.\n"
+	"   -l, --listen        Listen for new connections.\n"
+	"   -p, --port    PORT  Listen or connect on PORT (defaults to 1337).\n"
+	"   -h, --help          show this message and exit.\n";
 
 int main(int argc, char *argv[])
 {
 	// define long options
 	static struct option long_opts[] = {
-		{"connect",  required_argument, 0, 'c'},
-		{"listen",   no_argument,       0, 'l'},
-		{"port",     required_argument, 0, 'p'},
-		{"help",     no_argument,       0, 'h'},
-		{0,0,0,0}
-	};
+		{"connect", required_argument, 0, 'c'},
+		{"listen", no_argument, 0, 'l'},
+		{"port", required_argument, 0, 'p'},
+		{"help", no_argument, 0, 'h'},
+		{0, 0, 0, 0}};
 	// process options:
 	int c;
 	int opt_index = 0;
 	int port = 1337;
-	char hostname[HOST_NAME_MAX+1] = "localhost";
+	char hostname[HOST_NAME_MAX + 1] = "localhost";
 	hostname[HOST_NAME_MAX] = 0;
 	bool isclient = true;
 
-	while ((c = getopt_long(argc, argv, "c:lp:h", long_opts, &opt_index)) != -1) {
-		switch (c) {
-			case 'c':
-				if (strnlen(optarg,HOST_NAME_MAX))
-					strncpy(hostname,optarg,HOST_NAME_MAX);
-				break;
-			case 'l':
-				isclient = false;
-				break;
-			case 'p':
-				port = atoi(optarg);
-				break;
-			case 'h':
-				printf(usage,argv[0]);
-				return 0;
-			case '?':
-				printf(usage,argv[0]);
-				return 1;
+	while ((c = getopt_long(argc, argv, "c:lp:h", long_opts, &opt_index)) != -1)
+	{
+		switch (c)
+		{
+		case 'c':
+			if (strnlen(optarg, HOST_NAME_MAX))
+				strncpy(hostname, optarg, HOST_NAME_MAX);
+			break;
+		case 'l':
+			isclient = false;
+			break;
+		case 'p':
+			port = atoi(optarg);
+			break;
+		case 'h':
+			printf(usage, argv[0]);
+			return 0;
+		case '?':
+			printf(usage, argv[0]);
+			return 1;
 		}
 	}
-	if (isclient) {
-		initClientNet(hostname,port);
-	} else {
+	if (isclient)
+	{
+		initClientNet(hostname, port);
+	}
+	else
+	{
 		initServerNet(port);
 	}
 
@@ -387,35 +407,39 @@ int main(int argc, char *argv[])
 	init_ncurses();
 	init_readline();
 	/* start curses thread */
-	if (pthread_create(&tcurses,0,cursesthread,0)) {
+	if (pthread_create(&tcurses, 0, cursesthread, 0))
+	{
 		fprintf(stderr, "Failed to create curses thread.\n");
 	}
 	/* start receiver thread: */
-	if (pthread_create(&trecv,0,recvMsg,0)) {
+	if (pthread_create(&trecv, 0, recvMsg, 0))
+	{
 		fprintf(stderr, "Failed to create update thread.\n");
 	}
 
 	/* put this in the queue to signal need for resize: */
-	redraw_data rd = {false,"","",NULL};
-	do {
+	redraw_data rd = {false, "", "", NULL};
+	do
+	{
 		int c = wgetch(cmd_win);
-		switch (c) {
-			case KEY_RESIZE:
-				pthread_mutex_lock(&qmx);
-				mq.push_back(rd);
-				pthread_cond_signal(&qcv);
-				pthread_mutex_unlock(&qmx);
-				break;
-				// Ctrl-L -- redraw screen
-			// case '\f':
-			// 	// Makes the next refresh repaint the screen from scratch
-			// 	/* XXX this needs to be done in the curses thread as well. */
-			// 	clearok(curscr,true);
-			// 	resize();
-			// 	break;
-			default:
-				input = c;
-				rl_callback_read_char();
+		switch (c)
+		{
+		case KEY_RESIZE:
+			pthread_mutex_lock(&qmx);
+			mq.push_back(rd);
+			pthread_cond_signal(&qcv);
+			pthread_mutex_unlock(&qmx);
+			break;
+			// Ctrl-L -- redraw screen
+		// case '\f':
+		// 	// Makes the next refresh repaint the screen from scratch
+		// 	/* XXX this needs to be done in the curses thread as well. */
+		// 	clearok(curscr,true);
+		// 	resize();
+		// 	break;
+		default:
+			input = c;
+			rl_callback_read_char();
 		}
 	} while (!should_exit);
 
@@ -433,15 +457,17 @@ int main(int argc, char *argv[])
 /* We'll need yet another thread to listen for incoming messages and
  * post them to the queue. */
 
-void* cursesthread(void* pData)
+void *cursesthread(void *pData)
 {
 	/* NOTE: these calls only worked from the main thread... */
 	// init_ncurses();
 	// init_readline();
-	while (true) {
+	while (true)
+	{
 		pthread_mutex_lock(&qmx);
-		while (mq.empty()) {
-			pthread_cond_wait(&qcv,&qmx);
+		while (mq.empty())
+		{
+			pthread_cond_wait(&qcv, &qmx);
 			/* NOTE: pthread_cond_wait will release the mutex and block, then
 			 * reaquire it before returning.  Given that only one thread (this
 			 * one) consumes elements of the queue, we probably don't have to
@@ -450,15 +476,21 @@ void* cursesthread(void* pData)
 		}
 		/* at this point, we have control of the queue, which is not empty,
 		 * so write all the messages and then let go of the mutex. */
-		while (!mq.empty()) {
+		while (!mq.empty())
+		{
 			redraw_data m = mq.front();
 			mq.pop_front();
-			if (m.win == cmd_win) {
+			if (m.win == cmd_win)
+			{
 				cmd_win_redisplay(m.resize);
-			} else if (m.resize) {
+			}
+			else if (m.resize)
+			{
 				resize();
-			} else {
-				msg_win_redisplay(false,m.msg,m.sender);
+			}
+			else
+			{
+				msg_win_redisplay(false, m.msg, m.sender);
 				/* Redraw input window to "focus" it (otherwise the cursor
 				 * will appear in the transcript which is confusing). */
 				cmd_win_redisplay(false);
@@ -469,22 +501,24 @@ void* cursesthread(void* pData)
 	return 0;
 }
 
-void* recvMsg(void*)
+void *recvMsg(void *)
 {
 	size_t maxlen = 256;
-	char msg[maxlen+1];
+	char msg[maxlen + 1];
 	ssize_t nbytes;
-	while (1) {
-		if ((nbytes = recv(sockfd,msg,maxlen,0)) == -1)
+	while (1)
+	{
+		if ((nbytes = recv(sockfd, msg, maxlen, 0)) == -1)
 			error("recv failed");
 		msg[nbytes] = 0; /* make sure it is null-terminated */
-		if (nbytes == 0) {
+		if (nbytes == 0)
+		{
 			/* signal to the main loop that we should quit: */
 			should_exit = true;
 			return 0;
 		}
 		pthread_mutex_lock(&qmx);
-		mq.push_back({false,msg,"Mr Thread",msg_win});
+		mq.push_back({false, msg, "Mr Thread", msg_win});
 		pthread_cond_signal(&qcv);
 		pthread_mutex_unlock(&qmx);
 	}
--- openssl-examples/examples.c
+++ openssl-examples/examples.c
@@ -13,11 +13,12 @@
 void sha_example()
 {
 	/* hash a string with sha256 */
-	char* message = "this is a test message :D";
+	char *message = "this is a test message :D";
 	unsigned char hash[32]; /* change 32 to 64 if you use sha512 */
-	SHA256((unsigned char*)message,strlen(message),hash);
-	for (size_t i = 0; i < 32; i++) {
-		printf("%02x",hash[i]);
+	SHA256((unsigned char *)message, strlen(message), hash);
+	for (size_t i = 0; i < 32; i++)
+	{
+		printf("%02x", hash[i]);
 	}
 	printf("\n");
 	/* you can check that this is correct by running
@@ -27,15 +28,16 @@ void sha_example()
 /* demonstrates HMAC */
 void hmac_example()
 {
-	char* hmackey = "asdfasdfasdfasdfasdfasdf";
+	char *hmackey = "asdfasdfasdfasdfasdfasdf";
 	unsigned char mac[64]; /* if using sha512 */
-	memset(mac,0,64);
-	char* message = "this is a test message :D";
-	HMAC(EVP_sha512(),hmackey,strlen(hmackey),(unsigned char*)message,
-			strlen(message),mac,0);
-	printf("hmac-512(\"%s\"):\n",message);
-	for (size_t i = 0; i < 64; i++) {
-		printf("%02x",mac[i]);
+	memset(mac, 0, 64);
+	char *message = "this is a test message :D";
+	HMAC(EVP_sha512(), hmackey, strlen(hmackey), (unsigned char *)message,
+		 strlen(message), mac, 0);
+	printf("hmac-512(\"%s\"):\n", message);
+	for (size_t i = 0; i < 64; i++)
+	{
+		printf("%02x", mac[i]);
 	}
 	printf("\n");
 }
@@ -46,31 +48,34 @@ void ctr_example()
 	unsigned char key[32];
 	size_t i;
 	/* setup dummy (non-random) key and IV */
-	for (i = 0; i < 32; i++) key[i] = i;
+	for (i = 0; i < 32; i++)
+		key[i] = i;
 	unsigned char iv[16];
-	for (i = 0; i < 16; i++) iv[i] = i;
+	for (i = 0; i < 16; i++)
+		iv[i] = i;
 	/* NOTE: in general you need t compute the sizes of these
 	 * buffers.  512 is an arbitrary value larger than what we
 	 * will need for our short message. */
 	unsigned char ct[512];
 	unsigned char pt[512];
 	/* so you can see which bytes were written: */
-	memset(ct,0,512);
-	memset(pt,0,512);
-	char* message = "this is a test message :D";
+	memset(ct, 0, 512);
+	memset(pt, 0, 512);
+	char *message = "this is a test message :D";
 	size_t len = strlen(message);
 	/* encrypt: */
-	EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-	if (1!=EVP_EncryptInit_ex(ctx,EVP_aes_256_ctr(),0,key,iv))
+	EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+	if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_ctr(), 0, key, iv))
 		ERR_print_errors_fp(stderr);
 	int nWritten; /* stores number of written bytes (size of ciphertext) */
-	if (1!=EVP_EncryptUpdate(ctx,ct,&nWritten,(unsigned char*)message,len))
+	if (1 != EVP_EncryptUpdate(ctx, ct, &nWritten, (unsigned char *)message, len))
 		ERR_print_errors_fp(stderr);
 	EVP_CIPHER_CTX_free(ctx);
 	size_t ctlen = nWritten;
-	printf("ciphertext of length %i:\n",nWritten);
-	for (i = 0; i < ctlen; i++) {
-		printf("%02x",ct[i]);
+	printf("ciphertext of length %i:\n", nWritten);
+	for (i = 0; i < ctlen; i++)
+	{
+		printf("%02x", ct[i]);
 	}
 	printf("\n");
 	/* now decrypt.  NOTE: in counter mode, encryption and decryption are
@@ -79,13 +84,13 @@ void ctr_example()
 	 * Won't be an issue for our hybrid scheme as AES keys are only used
 	 * once.  */
 	/* wipe out plaintext to be sure it worked: */
-	memset(pt,0,512);
+	memset(pt, 0, 512);
 	ctx = EVP_CIPHER_CTX_new();
-	if (1!=EVP_DecryptInit_ex(ctx,EVP_aes_256_ctr(),0,key,iv))
+	if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), 0, key, iv))
 		ERR_print_errors_fp(stderr);
-	if (1!=EVP_DecryptUpdate(ctx,pt,&nWritten,ct,ctlen))
+	if (1 != EVP_DecryptUpdate(ctx, pt, &nWritten, ct, ctlen))
 		ERR_print_errors_fp(stderr);
-	printf("decrypted %i bytes:\n%s\n",nWritten,pt);
+	printf("decrypted %i bytes:\n%s\n", nWritten, pt);
 	/* NOTE: counter mode will preserve the length (although the person
 	 * decrypting needs to know the IV) */
 }
@@ -94,34 +99,40 @@ void ctr_example()
 void rsa_example()
 {
 	/* first generate a key */
-	RSA* keys = RSA_new();
-	if (!keys) exit(1);
-	BIGNUM* e = BN_new();
-	if (!e) exit(1);
-	BN_set_word(e,RSA_F4); /* e = 65537  */
+	RSA *keys = RSA_new();
+	if (!keys)
+		exit(1);
+	BIGNUM *e = BN_new();
+	if (!e)
+		exit(1);
+	BN_set_word(e, RSA_F4); /* e = 65537  */
 	/* NOTE: if you have an old enough openssl library, you might
 	 * have to setup the random number generator before this call: */
-	int r = RSA_generate_key_ex(keys,2048,e,NULL);
-	if (r != 1) exit(1);
-	PEM_write_RSA_PUBKEY(stdout,keys);
+	int r = RSA_generate_key_ex(keys, 2048, e, NULL);
+	if (r != 1)
+		exit(1);
+	PEM_write_RSA_PUBKEY(stdout, keys);
 	/* NOTE: you could fill in the last 5 parameters if you wanted to
 	 * password protect the private key. */
-	PEM_write_RSAPrivateKey(stdout,keys,NULL,NULL,0,NULL,NULL);
-	char* message = "this is a test message :D";
+	PEM_write_RSAPrivateKey(stdout, keys, NULL, NULL, 0, NULL, NULL);
+	char *message = "this is a test message :D";
 	size_t len = strlen(message);
-	unsigned char* ct = malloc(RSA_size(keys));
-	int ctlen = RSA_public_encrypt(len+1 /* include null char */, (unsigned char*)message,
-			ct, keys, RSA_PKCS1_OAEP_PADDING);
-	if (ctlen == -1) exit(1);
+	unsigned char *ct = malloc(RSA_size(keys));
+	int ctlen = RSA_public_encrypt(len + 1 /* include null char */, (unsigned char *)message,
+								   ct, keys, RSA_PKCS1_OAEP_PADDING);
+	if (ctlen == -1)
+		exit(1);
 	/* print RSA ciphertext */
 	printf("RSA ciphertext:\n");
-	for (int i = 0; i < ctlen; i++) printf("%02x",ct[i]);
+	for (int i = 0; i < ctlen; i++)
+		printf("%02x", ct[i]);
 	printf("\n");
 	/* now try to decrypt */
-	char* pt = malloc(ctlen);
-	size_t ptlen = RSA_private_decrypt(ctlen,ct,(unsigned char*)pt,keys,RSA_PKCS1_OAEP_PADDING);
-	if (ptlen == -1) exit(1);
-	printf("RSA decrypted plaintext:\n%s\n",pt);
+	char *pt = malloc(ctlen);
+	size_t ptlen = RSA_private_decrypt(ctlen, ct, (unsigned char *)pt, keys, RSA_PKCS1_OAEP_PADDING);
+	if (ptlen == -1)
+		exit(1);
+	printf("RSA decrypted plaintext:\n%s\n", pt);
 }
 
 /* TODO: add signature example  */
